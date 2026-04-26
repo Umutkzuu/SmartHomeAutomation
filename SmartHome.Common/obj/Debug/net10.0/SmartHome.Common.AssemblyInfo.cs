14f624b02fb98bbb588b044ee1d66503d8dff323
Umut Kuzu <umut@Umut-Air.home>
2026-04-26 16:17:10 +0300

    feat: Entegrasyon ayarları yapıldı farklı cihaz ayarları

--- SmartHome.Common/obj/Debug/net10.0/SmartHome.Common.AssemblyInfo.cs
+++ SmartHome.Common/obj/Debug/net10.0/SmartHome.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartHome.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a233a154c14464182d6799f4eecc6639ad555c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3ae828bfe114acb12f643462303472a9c729f4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartHome.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartHome.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SmartHome.Web/obj/Debug/net10.0/SmartHome.Web.AssemblyInfo.cs
+++ SmartHome.Web/obj/Debug/net10.0/SmartHome.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartHome.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1773566b31d7bad31ead67675a2b7dde4ea34f88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3ae828bfe114acb12f643462303472a9c729f4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartHome.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartHome.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
